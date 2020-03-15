4042dcc136ac880ab051d86a20dfaf59c894c871
Badr Choubai <badrchoubai@outlook.com>
2020-03-15 14:35:42 -0600

    minor

--- myHn.js
+++ myHn.js
@@ -3,15 +3,9 @@ var head = document.head || document.getElementsByTagName("head")[0],
 
 style.type = "text/css";
 style.innerHTML = `
-    body,
-    #hnmain {
-        background: #2f2f2f;
-    }
-
-    a.storylink,
-    a.morelink {
-        color: #fff;
-    }
+    body, #hnmain { background: #272822; }
+	a.storylink, a.morelink, span.commtext { color: #fff; }
+	a.storylink:visited { color: #444444; }
 `;
 
 head.appendChild(style);
