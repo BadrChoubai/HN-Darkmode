851bca36c77124d0d254b238d399cf638b366bf3
Badr Choubai <badrchoubai@outlook.com>
2020-04-21 18:34:42 -0600

    (minor)

--- hnDarkMode.js
+++ hnDarkMode.js
@@ -1,12 +1,35 @@
-var head = document.head || document.getElementsByTagName("head")[0],
+const getAttributes = () => {
+  let head = document.head || document.getElementsByTagName("head")[0];
+  let storyLinks = document.getElementsByClassName("storylink");
+
+  return {
+    head,
+    storyLinks,
+  };
+};
+
+const changeStyles = (targetElement) => {
   style = document.createElement("style");
 
-style.type = "text/css";
-style.innerHTML = `
+  // set style.innerHTML to darker colors, append to head
+  style.type = "text/css";
+  style.innerHTML = ` 
     body, #hnmain { background: #272822; }
-	a.storylink, a.morelink, span.commtext { color: #fff; }
-	a.storylink:visited { color: #444444; }
-`;
+    a.storylink, a.morelink, span.commtext { color: #fff; }
+    a.storylink:visited { color: #444444; }
+  `;
+  targetElement.appendChild(style);
+};
 
-head.appendChild(style);
+const changeLinkBehavior = (storyLinks) => {
+  // Target all links, add target attribute, set to 'blank'
+  Array.from(storyLinks).forEach((storyLink) =>
+    storyLink.setAttribute("target", "blank")
+  );
+};
 
+(function main() {
+  const { ...attributes } = getAttributes();
+  changeStyles(attributes.head);
+  changeLinkBehavior(attributes.storyLinks);
+})();
